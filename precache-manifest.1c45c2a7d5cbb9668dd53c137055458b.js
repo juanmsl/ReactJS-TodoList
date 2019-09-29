dc712914f8085286206a1ca43e5ad82b53b1c536
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2019-09-29 14:14:57 -0500

    Updates

---
+++ precache-manifest.1c45c2a7d5cbb9668dd53c137055458b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afae7fc5b00287a6a3ab6c3b326ef600",
+    "url": "/ReactJS-TodoList/index.html"
+  },
+  {
+    "revision": "aebc7473cd6d676ed312",
+    "url": "/ReactJS-TodoList/static/css/main.167d571b.chunk.css"
+  },
+  {
+    "revision": "810ec8f44c2bdb329601",
+    "url": "/ReactJS-TodoList/static/js/2.1fac3351.chunk.js"
+  },
+  {
+    "revision": "aebc7473cd6d676ed312",
+    "url": "/ReactJS-TodoList/static/js/main.90cb383e.chunk.js"
+  },
+  {
+    "revision": "72bd74c7751399b98734",
+    "url": "/ReactJS-TodoList/static/js/runtime-main.a1052b42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.474bb38c423010b418d4c9900a6fa219.js"
+  "/ReactJS-TodoList/precache-manifest.1c45c2a7d5cbb9668dd53c137055458b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactJS-TodoList/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
